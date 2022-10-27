28ce12177e7dfd7c9aadf96497dec9ae507d4bc6
Werozel <khaibulaev.mn@gmail.com>
2022-10-27 15:04:17 +0300

    Added point generation in master

--- main.cpp
+++ main.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
-#include "mpi.h"
+#include <mpi.h>
+#include <cmath>
+
+double a1 = 0, a2 = 0, a3 = 0, b1 = 1, b2 = 1, b3 = 1;
+
+const int NUMBER_OF_POINTS = 1000000;
+
+struct Point {
+    double x, y, z;
+
+    Point(double x, double y, double z) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+};
+
+double f(struct Point *point) {
+    return sqrt(point->x * point->x + point->y * point->y);
+}
+
+double fRand(double fMin, double fMax)
+{
+    double f = (double)rand() / RAND_MAX;
+    return fMin + f * (fMax - fMin);
+}
+
+void master_routine() {
+    srand(time(nullptr));
+
+    Point *points = (Point *) malloc(NUMBER_OF_POINTS * sizeof(Point) + MPI_BSEND_OVERHEAD + 7);
+    for (int i = 0; i < NUMBER_OF_POINTS; i++) {
+        double x = fRand(a1, b1);
+        double y = fRand(a2, b2);
+        double z = fRand(a3, b3);
+
+        points[i] = Point(x, y, z);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << points[i].x << " " << points[i].y << " " << points[i].z << std::endl;
+    }
+}
 
 int main(int argc, char *argv[]) {
     int size, rank;
@@ -9,8 +51,7 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     if (!rank) {
-        printf("Hello world\n");
-        fflush(stdin);
+        master_routine();
     } else {
         printf("Hello from %d\n", rank);
         fflush(stdin);
